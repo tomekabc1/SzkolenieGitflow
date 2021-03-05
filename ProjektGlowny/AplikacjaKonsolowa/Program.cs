00549e71d2f53feaf0aff6a67836748eee5cff45
MF\DNIY <tomasz.bartczak@mf.gov.pl>
2021-03-05 10:12:39 +0100

    init2

---
+++ ProjektGlowny/AplikacjaKonsolowa/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AplikacjaKonsolowa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
